ab5bb8863d621e0710f2c57761c7a9f030d03700
JChan91 <fourbin1221@gmail.com>
2020-11-29 19:34:57 +0900

    :sparkles: 로그인 API

--- controller/user/signin.js
+++ controller/user/signin.js
@@ -1,5 +1,20 @@
+const { User } = require('../../models');
+
 module.exports = {
-  post: (req, res) => {
-    res.status(200).send('반가워요 :) 여기는 로그인 페이지입니다.');
+  post: async (req, res) => {
+    const { email, password } = req.body;
+    console.log(req.body);
+    const userData = await User.findOne({ where: { email, password } });
+
+    try {
+      if (userData === null) {
+        res.status(404).send('이메일 또는 비밀번호가 잘못되었습니다.');
+      } else {
+        req.session.userid = userData.id;
+        res.status(200).json('로그인을 하였습니다.');
+      }
+    } catch (err) {
+      res.sendStatus(500);
+    }
   }
 }
