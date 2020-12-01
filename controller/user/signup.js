ae7448870001f17e97ab3facebe9d86abfccb8eb
JChan91 <fourbin1221@gmail.com>
2020-12-01 15:14:09 +0900

    :sparkles: 로그인 API 기능 추가

--- controller/user/signup.js
+++ controller/user/signup.js
@@ -1,5 +1,31 @@
+const { User } = require('../../models');
+
 module.exports = {
-  post: (req, res) => {
-    res.status(200).send('반가워요 :) 여기는 회원가입 페이지입니다.');
+  post: async (req, res) => {
+    const { email, nickname, mobile, password, profilePath, loginPlatform } = req.body;
+
+    if (!email || !nickname || !mobile || !password || !loginPlatform) {
+      res.status(422).send('정보를 다 입력해주세요');
+    }
+
+    const newUser = await User
+      .findOrCreate({
+        where: { email, nickname },
+        defaults: { mobile: mobile, password: password, loginPlatform: loginPlatform, profilePath: profilePath }
+      });
+
+    if (newUser) {
+      const [user, created] = newUser;
+
+      if (!created) {
+        res.status(409).send('이미 존재하는 유저입니다.');
+      }
+
+      try {
+        res.status(201).json(user);
+      } catch (err) {
+        res.sendStatus(500);
+      }
+    }
   }
 }
