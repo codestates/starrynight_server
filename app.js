acbf975169075e1c45beab482f19c6a5dde7764f
JChan91 <fourbin1221@gmail.com>
2020-12-07 04:52:24 +0900

    :sparkles: 사용자 비밀번호 찾기 API 작성 및 nodemailer 미들웨어 추가

--- app.js
+++ app.js
@@ -6,12 +6,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 // middleware
-app.use(cors({
-  origin: true,
-  credentials: true,
-})
-);
-
+app.use(cors({ origin: true, credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- controller/user/findUser.js
+++ controller/user/findUser.js
@@ -1,9 +1,47 @@
+const { User } = require('../../models');
+const nodeMailer = require('nodemailer');
+
 module.exports = {
   email: (req, res) => {
+    // 연락처로 id로 이메일 찾기
     res.status(200).send('반가워요 :) 여기는 사용자 찾기 중 이메일 찾는 페이지입니다.');
   },
 
-  password: (req, res) => {
-    res.status(200).send('반가워요 :) 여기는 사용자 찾기 중 비밀번호 찾는 페이지입니다.');
+  password: async (req, res) => {
+    const { email, mobile } = req.body;
+
+    // 연락처와 이메일을 받아 저장된 DB와 비교
+    const findUser = User.findOne({ where: { email: email, mobile: mobile } });
+
+    if (findUser) {
+      // 비밀번호를 변경(update)후 이메일로 변경된 비밀번호를 발송
+      const updatePassword = User.update(
+        { password: 1231 },
+        { where: { id: findUser.id } }
+      );
+
+      const transporter = nodeMailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.USER,
+          pass: process.env.PASS
+        }
+      });
+
+      const mailOptions = {
+        from: process.env.USER,
+        to: findUser.email,
+        subject: '[Starry Night] 비밀번호 찾기에 대한 메일입니다.',
+        text: '비밀번호가 : 1231 로 변경되었습니다.'
+      };
+
+      transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(200).send('변경된 비밀번호를 저장된 메일로 전송했습니다.');
+        }
+      })
+    }
   }
 }
