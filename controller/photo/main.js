0ef4e969ee0fecd6d41293ce792267f9c59b9886
JChan91 <fourbin1221@gmail.com>
2020-12-06 10:55:11 +0900

    :poop: 코드 들여쓰기 수정

--- controller/photo/main.js
+++ controller/photo/main.js
@@ -1,8 +1,8 @@
 module.exports = {
-  get: async (req, res) => {
-        console.log('여기는 메인페이지 API입니다');
-	console.log(req.headers);
-	res.sendStatus(200);
-  }
+	get: async (req, res) => {
+		console.log('여기는 메인페이지 API입니다');
+		console.log(req.headers);
+		res.sendStatus(200);
+	}
 };
 
