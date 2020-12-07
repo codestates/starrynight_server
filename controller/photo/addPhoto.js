b0371a400b6cd2a7d4368d86d9fc8871fdf6f8b0
Siwoo Baek <whitelatte46@gmail.com>
2020-12-07 21:49:57 +0900

    :memo: AWS S3 저장 및 URL 생성 기능개발 완료
    addPhoto, savePhoto controller 세팅 완료

--- controller/photo/addPhoto.js
+++ controller/photo/addPhoto.js
@@ -1,5 +1,17 @@
 module.exports = {
-  post: (req, res) => {
-    res.status(200).send("반가워요 :) 여기는 사진 삭제 페이지입니다.");
+  post: async (req, res) => {
+    try {
+      console.log("req.file: ", req.file);
+
+      let payLoad = {
+        success: true,
+        url: req.file.location, // S3에 저장된 파일의 최종 url
+        fileName: req.file.originalname, // S3에 저장된 파일의 최종 name
+      };
+      res.status(200).json(payLoad);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ success: false });
+    }
   },
 };
--- routes/photos.js
+++ routes/photos.js
@@ -1,24 +1,47 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const multer = require('multer');
-const { photoController } = require('../controller');
+const multer = require("multer");
+const { photoController } = require("../controller");
 
-let storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, '../uploadImg/');
-  },
-  filename: (req, file, callback) => {
-    callback(null, file.originalname);
-  }
+// S3 객체 활용을 위해 aws-sdk 로드
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
+aws.config.loadFromPath(__dirname + "/../config/awsconfig.json");
+
+let s3 = new aws.S3();
+
+let upload = multer({
+  // S3 저장 규칙
+  storage: multerS3({
+    // s3 bucket 연동내용
+    s3: s3,
+    bucket: "mystar-story.com",
+
+    // 업로드 파일명 부여(경로명 끝에 JPG 파일포맷을 추가)
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString() + ".jpg");
+    },
+
+    // 용량제한 설정
+    contentLength: 500000000,
+
+    // 업로드 파일들에 대한 권한 설정
+    acl: "public-read-write",
+
+    // content-type 범위 설정: 아무나 드루온나
+    // (...는 훼이크고 클라이언트가 multipart/form-data로 보내주기로 하였읍니다.)
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+  }),
 });
 
-let upload = multer({ storage: storage });
-
-router.get('/', photoController.intro.get);
-router.get('/main', photoController.main.get);
-router.post('/addphoto', upload.single('img'), photoController.addPhoto.post);
-router.get('/:id', photoController.pickPhoto.get);
-router.patch('/:id/modify', photoController.modifyPhoto.patch);
-router.delete('/:id/delete', photoController.deletePhoto.delete);
+// 가자! 아침의 node.js!
+// ~MVC에 대한 열망을 가득 담아~
+router.get("/", photoController.intro.get);
+router.get("/main", photoController.main.get);
+router.post("/addphoto", upload.single("file"), photoController.addPhoto.post);
+router.post("/savephoto", photoController.savePhoto.post);
+router.get("/:id", photoController.pickPhoto.get);
+router.patch("/:id/modify", photoController.modifyPhoto.patch);
+router.delete("/:id/delete", photoController.deletePhoto.delete);
 
 module.exports = router;
