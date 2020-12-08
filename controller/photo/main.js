34b5855a2b80e1d3d95b511100768abec72e3ab4
JChan91 <fourbin1221@gmail.com>
2020-12-08 20:51:04 +0900

    :bug: OAuth 수정

--- controller/photo/main.js
+++ controller/photo/main.js
@@ -1,6 +1,6 @@
 module.exports = {
 	get: async (req, res) => {
-		if (req.headers.authorization) {
+		if (req.headers.authorization === undefined) {
 			res.status(404).json('토큰이 없습니다!!');
 		} else {
 			res.status(200).json(req.headers.authorization);
