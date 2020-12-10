f5060bc36b00bfd2fab1f59171777b9745767c85
JChan91 <fourbin1221@gmail.com>
2020-12-10 21:24:21 +0900

    :sparkles: 사진 선택 API

--- controller/photo/pickPhoto.js
+++ controller/photo/pickPhoto.js
@@ -3,14 +3,39 @@ const { User, Photo, Reply } = require('../../models');
 module.exports = {
   get: async (req, res) => {
     const { id } = req.body;
-    const photoInfo = await Photo.findOne({ where: { hashtagId: id } });
-    const commentUser = await Reply.findAll({ where: { photoId: id } });
+    const photoInfo = await Photo.findOne({ where: { id: id } }); // hasttagId를 id로 변경
+    const commentId = await Reply.findAll({
+      where: { photoId: id },
+      attributes: ['commentId', 'comment']
+    });
 
-    // commentUser 중복 제거한 뒤에 있는 유저들의 Nickname 뽑아오기 .. new Set?
+    let writer = await User.findOne({ where: { id: photoInfo.dataValues.userId } });
+    let users = [];
+    let datas = {};
 
-    //console.log('사진 정보 : ', photoInfo);
-    console.log('사진 댓글 정보 : ', commentUser[0]);
+    // 타겟 사진에 댓글을 단 Users
+    commentId.forEach(ele => {
+      users.push(ele.commentId);
+    });
 
-    res.sendStatus(200);
+    // usersId = [...new Set(users)];
+
+    for (let i = 0; i < users.length; i++) {
+      let user = await User.findOne({ where: { id: users[i] } });
+
+      // 댓글에 해당 유저의 닉네임을 삽입
+      commentId[i].dataValues['nickname'] = user.dataValues.nickname;
+    }
+
+    console.log(writer);
+    datas = {
+      id: photoInfo.dataValues.id,
+      writer: writer.dataValues.nickname,
+      wirterProfilePath: writer.dataValues.profilePath,
+
+      replys: [...commentId]
+    }
+
+    res.status(200).json(datas);
   }
 };
