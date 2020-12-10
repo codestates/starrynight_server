266eae3f5ddcd9ad92a86829872b6d2bf36f40f9
JChan91 <fourbin1221@gmail.com>
2020-12-10 19:35:43 +0900

    :seedling: Photo Seed 파일 생성

---
+++ seeders/20201210071008-Photo.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    let dummyPhotos = [];
+    let dummyUser;
+
+    // userId, photoTitle, hashtagId, photoPath, location
+    for (let i = 1; i <= 30; i++) {
+      if (i % 2 === 0) {
+        dummyUser = 1;
+      } else {
+        dummyUser = 2;
+      }
+
+      let photoData = {
+        userId: dummyUser,
+        photoTitle: `Test Photo${i}`,
+        hashtagId: i,
+        photoPath: `https://s3.ap-northeast-2.amazonaws.com/mystar-story.com/uploadPhotos/img${i}.jpg`,
+        location: `한국의 어딘가 ${i}`,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+      dummyPhotos.push(photoData);
+    }
+    return queryInterface.bulkInsert('Photos', dummyPhotos, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Photos', null, {});
+  }
+};
