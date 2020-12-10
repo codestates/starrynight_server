fdfb8aaaaa8e101a314fce7d23a34b8bd92f6019
JChan91 <fourbin1221@gmail.com>
2020-12-10 15:37:48 +0900

    fix

---
+++ seeders/20201210043518-Hashtag.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    let dummyComments = [];
+    let dummyPhotoIdnUsers = 1;
+
+    for (let i = 1; i <= 10; i++) {
+      if (i % 2 === 0) {
+        dummyPhotoIdnUsers = 1;
+      } else {
+        dummyPhotoIdnUsers = 2;
+      }
+      let commentData = {
+        photoId: dummyPhotoIdnUsers,
+        commentId: dummyPhotoIdnUsers,
+        comment: `멋져요!! ${i}`,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+      dummyComments.push(commentData);
+    }
+    return queryInterface.bulkInsert('Replies', dummyComments, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Replies', null, {});
+  }
+};
