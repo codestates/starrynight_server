32485b5051a604ea79446a70d365334d00bdf9dd
Siwoo Baek <whitelatte46@gmail.com>
2020-12-10 17:50:18 +0900

    commit before merge

--- models/reply.js
+++ models/reply.js
@@ -1,23 +1,24 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Reply extends Model {
     static associate(models) {
       Reply.belongsTo(models.Photo, {
-        foreignKey: 'photoId',
-        targetKey: 'id'
+        foreignKey: "photoId",
+        targetKey: "id",
       });
-    };
-  };
-  Reply.init({
-    photoId: DataTypes.INTEGER,
-    commentId: DataTypes.INTEGER,
-    comment: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Reply',
-  });
+    }
+  }
+  Reply.init(
+    {
+      photoId: DataTypes.INTEGER,
+      commentId: DataTypes.INTEGER,
+      comment: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Reply",
+    }
+  );
   return Reply;
 };
