3eb79c6ff7167ae79ea46f6df2bd99715f54aa4b
Siwoo Baek <whitelatte46@gmail.com>
2020-12-16 11:44:28 +0900

    fix bug

--- controller/photo/cancelComment.js
+++ controller/photo/cancelComment.js
@@ -6,7 +6,7 @@ module.exports = {
   post: async (req, res) => {
     try {
       // 클라이언트로부터 로그인 토큰정보, 댓글을 받아온다
-      const { comment } = req.body;
+      const { comment, photoId } = req.body;
       const userToken = req.headers.authorization;
 
       // Token을 decoding 한다
@@ -15,24 +15,20 @@ module.exports = {
 
       // 인스턴스들 중 사용자가 취소하고자 하는 좋아요를 확인한다
       const wasReplied = await Reply.findOne({
-        where: { writerId: decode.id, comment: comment },
+        where: { writerId: decode.id, comment: comment, photoId: photoId },
       });
 
-      const photo = await Reply.findOne({
-        where: { id: wasReplied.photoId },
-      });
-
-      // 댓글정보를 null로 초기화한다
+      // 댓글정보를 삭제한다
       const delReply = await Reply.destory({
         where: {
           writerId: wasReplied.writerId,
-          comment: comment,
-          photoId: photo.id,
+          comment: wasReplied.comment,
+          photoId: wasReplied.photoId,
         },
       });
 
       // 댓글삭제 성공 시 success: true 값을 보내준다
-      res.status(201).json({ success: true });
+      res.status(201).json({ ...delReply, success: true });
     } catch (err) {
       // 댓글 생성 실패 시 success: false 값을 보내준다
       res.status(500).json({ success: false });
--- controller/photo/cancelLike.js
+++ controller/photo/cancelLike.js
@@ -6,7 +6,7 @@ module.exports = {
   post: async (req, res) => {
     try {
       // 클라이언트로부터 로그인 토큰정보, 좋아요 누른 사진의 경로를 받아온다
-      const { photoPath } = req.body;
+      const { photoId } = req.body;
       const userToken = req.headers.authorization;
 
       // Token을 decoding 한다
@@ -15,16 +15,12 @@ module.exports = {
 
       // 인스턴스들 중 사용자가 취소하고자 하는 좋아요를 확인한다
       const wasFavor = await Favorite.findOne({
-        where: { pickerId: decode.id },
-      });
-
-      const photo = await Photo.findOne({
-        where: { id: wasFavor.photoId },
+        where: { pickerId: decode.id, photoId: photoId },
       });
 
       // 사진정보(photoId)를 삭제한다
       const cancelLike = await Favorite.destroy({
-        where: { pickerId: wasFavor.pickerId, photoId: photo.id },
+        where: { pickerId: wasFavor.pickerId, photoId: wasFavor.id },
       });
 
       // 좋아요취소 성공 시 success: true 값과 함께 좋아요취소 정보를 보내준다
