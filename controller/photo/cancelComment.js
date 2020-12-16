d2cbfc04ff4e4fe8eed0669906dd7763324ed3bc
JChan91 <fourbin1221@gmail.com>
2020-12-16 10:57:48 +0900

    Modify Reply Model

--- controller/photo/cancelComment.js
+++ controller/photo/cancelComment.js
@@ -19,7 +19,7 @@ module.exports = {
       });
 
       // 댓글정보를 null로 초기화한다
-      const delReply = await wasReplied.update(
+      const delReply = await Reply.update(
         { photoId: null, commentId: null, comment: null },
         { where: { writerId: wasReplied.writerId, comment: comment } }
       );
