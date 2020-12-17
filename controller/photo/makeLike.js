d9af2c84d4d32c2404c0dd9eabdcaefbc845b69d
Siwoo Baek <whitelatte46@gmail.com>
2020-12-18 00:58:49 +0900

    update logic

--- controller/photo/makeLike.js
+++ controller/photo/makeLike.js
@@ -1,4 +1,4 @@
-const { Favorite } = require("../../models");
+const { Favorite, Photo } = require("../../models");
 const jwt = require("jsonwebtoken");
 const KEY = process.env.SECRET_KEY;
 
@@ -6,7 +6,7 @@ module.exports = {
   post: async (req, res) => {
     try {
       // 클라이언트로부터 로그인 토큰정보, 좋아요 누른 사진의 경로를 받아온다
-      const { photoId, photoPath } = req.body;
+      const { photoId } = req.body;
       const userToken = req.headers.authorization;
 
       // Token을 decoding 한다
@@ -14,36 +14,33 @@ module.exports = {
       let decode = jwt.verify(token, KEY);
 
       // 새 좋아요 인스턴스를 만든다
-      const [newLike, created] = await Favorite.findOrCreate({
-        where: { pickerId: decode.id, photoId: photoId },
-        defaults: { favorite: true },
+      const findLike = await Favorite.findOne({
+        where: {
+          pickerId: decode.id,
+          photoId: photoId,
+        },
       });
 
-      // 화이팅
-      if (created) {
-        if (created === 0) {
-          Favorite.update(
-            { favorite: true },
-            {
-              where: {
-                pickerId: decode.id,
-                photoId: photoId,
-              },
-            }
-          );
-        } else {
-          Favorite.update(
-            { favorite: false },
-            {
-              where: {
-                pickerId: decode.id,
-                photoId: photoId,
-              },
-            }
-          );
-        }
+      if (!findLike) {
+        const newLike = await Favorite.create({
+          pickerId: decode.id,
+          photoId: photoId,
+          favorite: true,
+        });
+      } else {
+        const update = await Favorite.update(
+          { favorite: !findLike.dataValues.favorite },
+          {
+            where: {
+              pickerId: findLike.dataValues.pickerId,
+              photoId: findLike.dataValues.photoId,
+            },
+          }
+        );
       }
-      res.status(201).json("좋아요를 눌렀습니다 :)");
+
+      // 댓글 생성 성공 시 success: true 값을 보내준다
+      res.status(201).send("좋아요를 눌렀습니다 :)");
     } catch (err) {
       res.status(500).send("실패입니다");
     }
