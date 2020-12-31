5936b1b38da7ad35f99125dac495429838ba4c0e
JChan91 <fourbin1221@gmail.com>
2020-12-31 10:17:08 +0900

    :art: 탈퇴, 좋아요, 갤러리 Access Token 만료 시 토큰 재생성

--- controller/user/deleteUser.js
+++ controller/user/deleteUser.js
@@ -2,10 +2,30 @@ const { User } = require('../../models');
 const jwt = require('jsonwebtoken');
 const KEY = process.env.SECRET_KEY;
 
+function findAuthorized(res, acc, ref) {
+  // 모든 토큰이 없을 경우
+  if (!acc && !ref) {
+    res.status(401).send('다시 로그인 해주세요');
+  } else {
+    if (acc) {
+      return [acc];
+    } else {
+      // Access Token 재생성
+      let accessToken = jwt.sign(
+        { id: id },
+        process.env.SECRET_KEY,
+        { expiresIn: '1d' }
+      );
+
+      return [ref, accessToken];
+    }
+  }
+}
+
 module.exports = {
   delete: async (req, res) => {
     const token = req.headers.authorization;
-    const decode = jwt.verify(token, KEY);
+    const decode = jwt.verify(token[0], KEY);
 
     if (decode) {
       res.clearCookie('refreshToken');
